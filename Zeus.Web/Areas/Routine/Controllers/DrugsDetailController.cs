ffcb6896fe8ecf144a2dd31542871a4d9a056878
Rex <531483547@qq.com>
2017-12-12 08:39:56 +0800

    药品

---
+++ Zeus.Web/Areas/Routine/Controllers/DrugsDetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zeus.Web.Areas.Routine.Controllers
+{
+    public class DrugsDetailController : Controller
+    {
+        // GET: Routine/DrugsDetail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
